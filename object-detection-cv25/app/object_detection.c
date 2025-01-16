7201aa68c0afe65a58a8bef3aa48a9c73df61cc1
AAC\aac78 <atle.andersen@tellu.no>
2025-01-16 14:46:27 +0100

    draw furniture bounding boxes on cv25 wip

--- object-detection-cv25/app/object_detection.c
+++ object-detection-cv25/app/object_detection.c
@@ -50,6 +50,7 @@
  * Twelfth  argument, ANCHORSFILE, is a string describing path to the anchors.
  */
 
+#include <bbox.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -68,6 +69,17 @@
 #include "postprocessing.h"
 #include "vdo-frame.h"
 #include "vdo-types.h"
+
+// BEGIN BBOX GLOBALS
+#define BOUNDING_BOX_DISPLAY_COUNT   10
+#define BOUNDING_BOX_GREEN_THRESHOLD 0.2
+bbox_color_t BOUNDING_BOX_COLOR_RED;
+bbox_color_t BOUNDING_BOX_COLOR_GREEN;
+bbox_color_t BOUNDING_BOX_COLOR_BLUE;
+
+bbox_t* bounding_box = NULL;
+
+// END BBOX GLOBALS
 
 /**
  * @brief Free up resources held by an array of labels.
@@ -399,6 +411,311 @@ end:
     return ret;
 }
 
+// BEGIN BB
+// This example illustrates drawing on a single channel.
+// The coordinate-space equals the visible area of the chosen channel.
+//
+//    ┏━━━━━━━━━━━━━━━━━━━━━━━━┓
+//    ┃                        ┃
+//    ┃ [0,0]                  ┃
+//    ┃   ┏━━━━━━━━━━┓         ┃
+//    ┃   ┃          ┃         ┃
+//    ┃   ┃ Channel1 ┃         ┃
+//    ┃   ┃          ┃         ┃
+//    ┃   ┗━━━━━━━━━━┛         ┃
+//    ┃            [1,1]       ┃
+//    ┃                        ┃
+//    ┃                        ┃
+//    ┗━━━━━━━━━━━━━━━━━━━━━━━━┛
+//
+// The intended usecase is performing video content analytics on one channel,
+// then draw bounding boxes with the same coordinate-space as was used for
+// Video Content Analytics (VCA).
+/*
+static void example_single_channel(void) {
+    // Draw on a single view: 1
+    bbox_t* bbox = bbox_view_new(1u);
+    if (!bbox)
+        panic("Failed creating: %s", strerror(errno));
+
+    bbox_clear(bbox);  // Remove all old bounding-boxes
+
+    // Create all needed colors [These operations are slow!]
+    const bbox_color_t red   = bbox_color_from_rgb(0xff, 0x00, 0x00);
+    const bbox_color_t blue  = bbox_color_from_rgb(0x00, 0x00, 0xff);
+    const bbox_color_t green = bbox_color_from_rgb(0x00, 0xff, 0x00);
+
+    bbox_style_outline(bbox);                      // Switch to outline style
+    bbox_thickness_thin(bbox);                     // Switch to thin lines
+    bbox_color(bbox, red);                         // Switch to red [This operation is fast!]
+    bbox_rectangle(bbox, 0.05, 0.05, 0.95, 0.95);  // Draw a thin red outline rectangle
+
+    bbox_style_corners(bbox);                      // Switch to corners style
+    bbox_thickness_thick(bbox);                    // Switch to thick lines
+    bbox_color(bbox, blue);                        // Switch to blue [This operation is fast!]
+    bbox_rectangle(bbox, 0.40, 0.40, 0.60, 0.60);  // Draw thick blue corners
+
+    bbox_style_corners(bbox);                      // Switch to corners style
+    bbox_thickness_medium(bbox);                   // Switch to medium lines
+    bbox_color(bbox, blue);                        // Switch to blue [This operation is fast!]
+    bbox_rectangle(bbox, 0.30, 0.30, 0.50, 0.50);  // Draw medium blue corners
+
+    bbox_style_outline(bbox);   // Switch to outline style
+    bbox_thickness_thin(bbox);  // Switch to thin lines
+    bbox_color(bbox, red);      // Switch to red [This operation is fast!]
+
+    // Draw a thin red quadrilateral
+    bbox_quad(bbox, 0.10, 0.10, 0.30, 0.12, 0.28, 0.28, 0.11, 0.30);
+
+    // Draw a green polyline
+    bbox_color(bbox, green);  // Switch to green [This operation is fast!]
+    bbox_move_to(bbox, 0.2, 0.2);
+    bbox_line_to(bbox, 0.5, 0.5);
+    bbox_line_to(bbox, 0.8, 0.4);
+    bbox_draw_path(bbox);
+
+    // Draw all queued geometry simultaneously
+    if (!bbox_commit(bbox, 0u))
+        panic("Failed committing: %s", strerror(errno));
+
+    if (running)
+        sleep(5);
+    bbox_destroy(bbox);
+}
+*/
+
+// This example illustrates drawing on multiple channels.
+// The coordinate-space equals global (entire sensor).
+//
+//  [0,0]
+//    ┏━━━━━━━━━━━━━━━━━━━━━━━━┓
+//    ┃  Channel1              ┃
+//    ┃  ┏━━━━━━┓              ┃
+//    ┃  ┃      ┃              ┃
+//    ┃  ┃      ┃              ┃
+//    ┃  ┗━━━━━━┛              ┃
+//    ┃              Channel2  ┃
+//    ┃              ┏━━━━━━┓  ┃
+//    ┃              ┃      ┃  ┃
+//    ┃              ┃      ┃  ┃
+//    ┃              ┗━━━━━━┛  ┃
+//    ┗━━━━━━━━━━━━━━━━━━━━━━━━┛
+//                           [1,1]
+//
+// The intended usecase is performing video content analytics on the entire image,
+// then draw bounding boxes with the same coordinate-space as was used for VCA,
+// and have it appear in all chosen channels simultaneously.
+//
+// Note that if you instead run VCA once per channel, i.e. multiple images,
+// then you need to manually translate the coordinates to the global image space,
+// before they can be drawn.
+// static void example_multiple_channels(void) {
+//     // Draw on channel 1 and 2
+//     bbox_t* bbox = bbox_new(2u, 1u, 2u);
+//     if (!bbox)
+//         syslog(LOG_INFO, "Failed creating: %s", strerror(errno));
+
+//     // If camera lacks video output, this call will succeed but not do anything.
+//     if (!bbox_video_output(bbox, true))
+//         syslog(LOG_INFO, "Failed enabling video-output: %s", strerror(errno));
+
+//     // Create all needed colors [These operations are slow!]
+//     const bbox_color_t colors[] = {
+//         bbox_color_from_rgb(0xff, 0u, 0u),
+//         bbox_color_from_rgb(0u, 0xff, 0u),
+//         bbox_color_from_rgb(0u, 0u, 0xff),
+//     };
+
+//     const size_t ncolors = sizeof(colors) / sizeof(colors[0u]);
+
+//     // Switch to thick corner style
+//     bbox_thickness_thick(bbox);
+//     bbox_style_corners(bbox);
+
+//     const float w     = 1920.f;
+//     const float h     = 1080.f;
+//     const float box_w = 100.f / w;
+//     const float box_h = 100.f / h;
+
+//     // Draw 32 bounding-boxes
+//     for (size_t i = 0; i < 32; ++i) {
+//         const float x = (200u) * (i % 8u) / w;
+//         const float y = (200u) * (i / 8u) / h;
+
+//         // Switch color [This operation is fast!]
+//         bbox_color(bbox, colors[i % ncolors]);
+
+//         bbox_rectangle(bbox, x, y, x + box_w, y + box_h);
+//     }
+
+//     // Draw all queued geometry simultaneously
+//     if (!bbox_commit(bbox, 0u))
+//         syslog(LOG_INFO, "Failed committing: %s", strerror(errno));
+
+//     syslog(LOG_INFO, "BOUNDING BOX DRAWN. SLEEPING");
+//     g_usleep(5 * 1000000);
+
+//     bbox_destroy(bbox);
+// }
+// // END BB
+
+static box* sort_object_bounding_boxes(box* orig_boxes, size_t length) {
+    gboolean swapped;
+
+    box* boxes = malloc(sizeof(box) * length);
+    memcpy(boxes, orig_boxes, sizeof(box) * length);
+
+    for (size_t i = 0; i < length - 1; i++) {
+        swapped = FALSE;
+        for (size_t j = 0; j < length - i - 1; j++) {
+            if (boxes[j].score < boxes[j + 1].score) {
+                box tmp      = boxes[j];
+                boxes[j]     = boxes[j + 1];
+                boxes[j + 1] = tmp;
+
+                swapped = TRUE;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+
+    return boxes;
+}
+
+static void draw_object_bounding_boxes(box* unsorted_object_boxes,
+                                       size_t object_boxes_length,
+                                       unsigned int frame_width,
+                                       unsigned int frame_height,
+                                       char** labels) {
+    // Remove stuff from last frame (TODO: probably way better ways of doing this)
+    if (bounding_box) {
+        bbox_destroy(bounding_box);
+    }
+
+    // bounding_box = bbox_new(2u, 1u, 2u);
+    bounding_box = bbox_view_new(1u);
+    if (!bounding_box) {
+        syslog(LOG_INFO, "Failed creating bbox: %s", strerror(errno));
+    }
+
+    // If camera lacks video output, this call will succeed but not do anything.
+    if (!bbox_video_output(bounding_box, true)) {
+        syslog(LOG_INFO, "Failed enabling video-output for bbox: %s", strerror(errno));
+    }
+
+    // TODO: Not sure how much of this needs to be done on every draw call
+    // Draw on channel 1 and 2 (TODO: not sure what this means)
+
+    // bbox_coordinates_frame_normalized(bounding_box);  // TODO: not sure if this should be used
+
+    BOUNDING_BOX_COLOR_RED   = bbox_color_from_rgb(0xff, 0x0, 0x0);
+    BOUNDING_BOX_COLOR_GREEN = bbox_color_from_rgb(0x0, 0xff, 0x0);
+    BOUNDING_BOX_COLOR_BLUE  = bbox_color_from_rgb(0x0, 0x0, 0xff);
+
+    // Switch to thick corner style
+    bbox_thickness_thick(bounding_box);
+    bbox_style_corners(bounding_box);
+
+    syslog(LOG_INFO, "About to draw boxes: got %zd boxes", object_boxes_length);
+
+    box* object_boxes = sort_object_bounding_boxes(unsorted_object_boxes, object_boxes_length);
+
+    size_t box_count = 0;
+    size_t i         = 0;
+    while (i < object_boxes_length && box_count < 5) {  // TODO: fix
+        box* object_box = &object_boxes[i++];
+        char* label     = labels[object_box->label - 1];
+
+        if (object_box->label == 0 || object_box->score < 0.1) {
+            continue;
+            syslog(LOG_INFO, "GOT BOX WITH LABEL 0");
+        }
+
+        box_count++;
+
+        // Get coordinates
+        // Get normalized coords
+        float top    = object_box->y_min;
+        float left   = object_box->x_min;
+        float bottom = object_box->y_max;
+        float right  = object_box->x_max;
+
+        unsigned int cropped_frame_width = frame_height;
+
+        // Get screen coords
+        unsigned int crop_x = left * cropped_frame_width + (frame_width - frame_height) / 2;
+        unsigned int crop_y = top * frame_height;
+        unsigned int crop_w = (right - left) * cropped_frame_width;
+        unsigned int crop_h = (bottom - top) * frame_height;
+
+        // Normalize screen coords
+        float box_left   = crop_x / (float)frame_width;
+        float box_top    = crop_y / (float)frame_height;
+        float box_right  = (crop_x + crop_w) / (float)frame_width;
+        float box_bottom = (crop_y + crop_h) / (float)frame_height;
+
+        // // Get color based on score
+        // bbox_color_t color = object_box->score >= BOUNDING_BOX_GREEN_THRESHOLD
+        //                          ? BOUNDING_BOX_COLOR_GREEN
+        //                          : BOUNDING_BOX_COLOR_RED;
+
+        // Get color based on label
+        bbox_color_t color;
+        if (strcmp(label, "bed") == 0) {
+            color = BOUNDING_BOX_COLOR_GREEN;
+        } else if (strcmp(label, "chair") == 0) {
+            color = BOUNDING_BOX_COLOR_BLUE;
+        } else {
+            color = BOUNDING_BOX_COLOR_RED;
+        }
+
+        bbox_color(bounding_box, color);
+
+        syslog(LOG_INFO,
+               "Drawing bounding with index %zd score %f, label %s, color: %p, crop x/y/w/h: %d %d "
+               "%d %d, "
+               "box at l/t/r/b: l%f t%f r%f b%f",
+               i,
+               object_box->score,
+               labels[object_box->label - 1],
+               (void*)&color,
+               crop_x,
+               crop_y,
+               crop_w,
+               crop_h,
+               box_left,
+               box_top,
+               box_right,
+               box_bottom);
+
+        // bbox_rectangle(bounding_box,
+        //                left,
+        //                top,
+        //                right,
+        //                bottom);  // TODO: THIS ONE DEFINITELY DOESNT WORK: Try one below
+        bbox_rectangle(bounding_box, box_left, box_top, box_right, box_bottom);
+    }
+
+    // Draw 32 bounding-boxes
+    // for (size_t i = 0; i < 10; ++i) {
+    //     const float x = (200u) * (i % 8u) / frame_width;
+    //     const float y = (200u) * (i / 8u) / frame_height;
+
+    //     // Switch color [This operation is fast!]
+    //     bbox_color(bounding_box, BOUNDING_BOX_COLOR_GREEN);
+
+    //     bbox_rectangle(bounding_box, x, y, x + 30, y + 30);
+    // }
+
+    // Draw bounding boxes
+    if (!bbox_commit(bounding_box, 0u)) {
+        syslog(LOG_INFO, "Failed to draw bounding boxes: %s", strerror(errno));
+    }
+}
+
 /**
  * @brief Main function that starts a stream with different options.
  */
@@ -482,19 +799,31 @@ int main(int argc, char** argv) {
         goto earlyend;
     }
 
-    const char* chipString       = args.chip;
-    const char* modelFile        = args.modelFile;
-    const char* labelsFile       = args.labelsFile;
-    const int inputWidth         = args.width;
-    const int inputHeight        = args.height;
-    const int desiredHDImgWidth  = args.raw_width;
+    const char* chipString = args.chip;
+    const char* modelFile  = args.modelFile;
+    const char* labelsFile = args.labelsFile;
+    const int inputWidth   = args.width;
+    const int inputHeight  = args.height;
+    // Seems to be set to 640x360 by default?
+    const int desiredHDImgWidth  = args.raw_width;  // TODO: try changing to 1280
     const int desiredHDImgHeight = args.raw_height;
-    const int threshold          = args.threshold;
+    const int threshold          = 20;  // args.threshold;
     const int quality            = args.quality;
     const int numberOfDetections = args.numDetections;  // number of detections
     const int numberOfClasses    = args.numLabels;      // number of classes
     char* anchorFile             = args.anchorsFile;
     const int padding            = args.padding;
+
+    syslog(
+        LOG_INFO,
+        "HDImgWidth/Height %dx%d input width/height %dx%d numdetections %d padding: %d classes: %d",
+        desiredHDImgWidth,
+        desiredHDImgHeight,
+        inputWidth,
+        inputHeight,
+        numberOfDetections,
+        padding,
+        numberOfClasses);
 
     if (strcmp(chipString, "ambarella-cvflow") != 0) {
         syslog(LOG_ERR, "This example supports only cv25 device ");
@@ -508,8 +837,8 @@ int main(int argc, char** argv) {
 
     // Hard coding stream resolution because CV25 devices don't support well
     // the function to pick the best resolution
-    int streamWidth  = 640;
-    int streamHeight = 360;
+    int streamWidth  = 640;  // TODO: try changing to 1280?
+    int streamHeight = 360;  // TODO: try changing to 720?
     syslog(LOG_INFO,
            "Creating VDO image provider and creating stream %d x %d",
            streamWidth,
@@ -873,9 +1202,28 @@ int main(int argc, char** argv) {
     // This contains the box coordinates and class scores for each detected object.
     boxes = (box*)malloc(sizeof(box) * numberOfDetections);
 
+    // BEGIN INIT BBOX --------------------------
+    // BOUNDING_BOX_COLOR_RED   = bbox_color_from_rgb(0xff, 0x0, 0x0);
+    // BOUNDING_BOX_COLOR_GREEN = bbox_color_from_rgb(0x0, 0xff, 0x0);
+
+    // Draw on channel 1 and 2 (TODO: not sure what this means)
+    // bounding_box = bbox_new(2u, 1u, 2u);
+    // if (!bounding_box) {
+    //     syslog(LOG_INFO, "Failed creating bbox: %s", strerror(errno));
+    // }
+
+    // // If camera lacks video output, this call will succeed but not do anything.
+    // if (!bbox_video_output(bounding_box, true)) {
+    //     syslog(LOG_INFO, "Failed enabling video-output for bbox: %s", strerror(errno));
+    // }
+
+    // END INIT BBOX ----------------------------
+
     while (true) {
         struct timeval startTs, endTs;
         unsigned int elapsedMs = 0;
+
+        g_usleep(3 * 1000000);
 
         // Get latest frame from image pipeline.
         VdoBuffer* buf = getLastFrameBlocking(sdImageProvider);
@@ -980,6 +1328,8 @@ int main(int argc, char** argv) {
                        boxes);
         gettimeofday(&endTs, NULL);
 
+        draw_object_bounding_boxes(boxes, numberOfDetections, widthFrameHD, heightFrameHD, labels);
+
         elapsedMs = (unsigned int)(((endTs.tv_sec - startTs.tv_sec) * 1000) +
                                    ((endTs.tv_usec - startTs.tv_usec) / 1000));
         syslog(LOG_INFO, "Postprocesing in %u ms", elapsedMs);
@@ -997,15 +1347,21 @@ int main(int argc, char** argv) {
             unsigned int crop_h = (bottom - top) * heightFrameHD;
 
             if (boxes[i].score >= threshold / 100.0 && boxes[i].label != 0) {
-                syslog(LOG_INFO,
-                       "Object %d: Classes: %s - Scores: %f - Locations: [%f,%f,%f,%f]",
-                       i,
-                       labels[boxes[i].label - 1],
-                       boxes[i].score,
-                       top,
-                       left,
-                       bottom,
-                       right);
+                syslog(
+                    LOG_INFO,
+                    "Object %d: Classes: %s - Scores: %f - Locations: [t%f,l%f,b%f,r%f] normalized "
+                    "locations [%f,%f,%f,%f]",
+                    i,
+                    labels[boxes[i].label - 1],
+                    boxes[i].score,
+                    top,
+                    left,
+                    bottom,
+                    right,
+                    top,
+                    left,
+                    bottom,
+                    right);
 
                 unsigned char* crop_buffer = crop_interleaved(ppOutputAddrHD,
                                                               widthFrameHD,
@@ -1125,6 +1481,10 @@ end:
     if (boxes) {
         free(boxes);
     }
+
+    // BEGIN CLEAN BBOX
+    bbox_destroy(bounding_box);
+    // END CLEAN BBOX
 
 earlyend:
     syslog(LOG_INFO, "Exit %s", argv[0]);
